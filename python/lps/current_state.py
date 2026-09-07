4306d50757ff20c63aabd3a23cb97ba9c863c8d1
Rajesh Kannan <rrajeshkannan@users.noreply.github.com>
2026-09-08 00:32:35 +0530

    Point Current State at LPS Position

--- python/lps/current_state.py
+++ python/lps/current_state.py
@@ -5,7 +5,7 @@ from __future__ import annotations
 from dataclasses import dataclass
 from decimal import Decimal
 
-from cas_import_poc.models import Position
+from lps.positions import Position
 
 
 @dataclass(frozen=True)
