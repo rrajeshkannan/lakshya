16ced91ff23cdd8f27da37ff5213ff0e6e917a96
Rajesh Kannan <rrajeshkannan@users.noreply.github.com>
2026-09-19 20:03:35 +0530

    Export FIFO holding-lot evidence

--- python/lts/__init__.py
+++ python/lts/__init__.py
@@ -7,6 +7,7 @@ from .position_bridge import LtsPosition, LtsPositionId, bridge_positions, valid
 from .reconciliation import reconcile_economically, reconcile_positions
 from .physical_transaction_history import transactions_for_holding
 from .holding_history import HoldingHistory, holding_history
+from .lots import HoldingLot, fifo_holding_lots
 from .treatments import TransitionTreatment, classify_position_treatment
 
 __all__ = [
@@ -14,5 +15,7 @@ __all__ = [
     "TransitionEvidence", "TransitionEvidencePosition", "build_transition_evidence",
     "LtsPosition", "LtsPositionId", "bridge_positions", "validate_slice_percentages",
     "TargetFormation", "TransitionTreatment", "classify_position_treatment",
-    "build_formation_intent", "reconcile_economically", "reconcile_positions", "transactions_for_holding", "HoldingHistory", "holding_history",
+    "build_formation_intent", "reconcile_economically", "reconcile_positions",
+    "transactions_for_holding", "HoldingHistory", "holding_history",
+    "HoldingLot", "fifo_holding_lots",
 ]
