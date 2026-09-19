e22a9910c2b7c257ca28437aa211dffb0849b218
Rajesh Kannan <rrajeshkannan@users.noreply.github.com>
2026-09-19 14:20:51 +0530

    Export Formation Intent builder

--- python/lts/__init__.py
+++ python/lts/__init__.py
@@ -1,6 +1,7 @@
 """Lakshya Transition System domain and analytical components."""
 
 from .evidence import TransitionEvidence, TransitionEvidencePosition, build_transition_evidence
+from .formation_intent import build_formation_intent
 from .models import EconomicReconciliation, FormationIntentRow, PositionReconciliation, TargetFormation
 from .reconciliation import reconcile_economically, reconcile_positions
 from .treatments import TransitionTreatment, classify_position_treatment
@@ -9,5 +10,5 @@ __all__ = [
     "EconomicReconciliation", "FormationIntentRow", "PositionReconciliation",
     "TransitionEvidence", "TransitionEvidencePosition", "build_transition_evidence",
     "TargetFormation", "TransitionTreatment", "classify_position_treatment",
-    "reconcile_economically", "reconcile_positions",
+    "build_formation_intent", "reconcile_economically", "reconcile_positions",
 ]
