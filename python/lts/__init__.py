5f8f1970f23c94f131aaa257ab741692912a727e
Rajesh Kannan <rrajeshkannan@users.noreply.github.com>
2026-09-19 19:30:39 +0530

    Remove speculative acquisition classifier export

--- python/lts/__init__.py
+++ python/lts/__init__.py
@@ -7,7 +7,6 @@ from .position_bridge import LtsPosition, LtsPositionId, bridge_positions, valid
 from .reconciliation import reconcile_economically, reconcile_positions
 from .physical_transaction_history import transactions_for_holding
 from .holding_history import HoldingHistory, holding_history
-from .acquisition_transactions import ACQUISITION_EVENT_TYPES, acquisition_transactions_for_holding
 from .treatments import TransitionTreatment, classify_position_treatment
 
 __all__ = [
@@ -15,5 +14,5 @@ __all__ = [
     "TransitionEvidence", "TransitionEvidencePosition", "build_transition_evidence",
     "LtsPosition", "LtsPositionId", "bridge_positions", "validate_slice_percentages",
     "TargetFormation", "TransitionTreatment", "classify_position_treatment",
-    "build_formation_intent", "reconcile_economically", "reconcile_positions", "transactions_for_holding", "HoldingHistory", "holding_history", "ACQUISITION_EVENT_TYPES", "acquisition_transactions_for_holding",
+    "build_formation_intent", "reconcile_economically", "reconcile_positions", "transactions_for_holding", "HoldingHistory", "holding_history",
 ]
