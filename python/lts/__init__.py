495a557256b4f99a7225a304a9f7f6e6cf0c93ef
Rajesh Kannan <rrajeshkannan@users.noreply.github.com>
2026-09-19 18:47:53 +0530

    Export physical holding transaction history

--- python/lts/__init__.py
+++ python/lts/__init__.py
@@ -5,6 +5,7 @@ from .formation_intent import build_formation_intent
 from .models import EconomicReconciliation, FormationIntentRow, PositionReconciliation, TargetFormation
 from .position_bridge import LtsPosition, LtsPositionId, bridge_positions, validate_slice_percentages
 from .reconciliation import reconcile_economically, reconcile_positions
+from .physical_transaction_history import transactions_for_holding
 from .treatments import TransitionTreatment, classify_position_treatment
 
 __all__ = [
@@ -12,5 +13,5 @@ __all__ = [
     "TransitionEvidence", "TransitionEvidencePosition", "build_transition_evidence",
     "LtsPosition", "LtsPositionId", "bridge_positions", "validate_slice_percentages",
     "TargetFormation", "TransitionTreatment", "classify_position_treatment",
-    "build_formation_intent", "reconcile_economically", "reconcile_positions",
+    "build_formation_intent", "reconcile_economically", "reconcile_positions", "transactions_for_holding",
 ]
