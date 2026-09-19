369c95acbd7c33ecb817672455dd1a63fd8668a9
Rajesh Kannan <rrajeshkannan@users.noreply.github.com>
2026-09-19 18:44:22 +0530

    Export LTS Slice invariant validator

--- python/lts/__init__.py
+++ python/lts/__init__.py
@@ -3,14 +3,14 @@
 from .evidence import TransitionEvidence, TransitionEvidencePosition, build_transition_evidence
 from .formation_intent import build_formation_intent
 from .models import EconomicReconciliation, FormationIntentRow, PositionReconciliation, TargetFormation
-from .position_bridge import LtsPosition, LtsPositionId, bridge_positions
+from .position_bridge import LtsPosition, LtsPositionId, bridge_positions, validate_slice_percentages
 from .reconciliation import reconcile_economically, reconcile_positions
 from .treatments import TransitionTreatment, classify_position_treatment
 
 __all__ = [
     "EconomicReconciliation", "FormationIntentRow", "PositionReconciliation",
     "TransitionEvidence", "TransitionEvidencePosition", "build_transition_evidence",
-    "LtsPosition", "LtsPositionId", "bridge_positions",
+    "LtsPosition", "LtsPositionId", "bridge_positions", "validate_slice_percentages",
     "TargetFormation", "TransitionTreatment", "classify_position_treatment",
     "build_formation_intent", "reconcile_economically", "reconcile_positions",
 ]
