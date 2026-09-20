c5d0ca0123545e0ba2396da90c8bdf016a0007a2
Rajesh Kannan <rrajeshkannan@users.noreply.github.com>
2026-09-20 20:50:04 +0530

    Expose FIFO redemption allocation evidence

--- python/lts/__init__.py
+++ python/lts/__init__.py
@@ -9,6 +9,7 @@ from .reconciliation import reconcile_economically, reconcile_positions
 from .physical_transaction_history import transactions_for_holding
 from .holding_history import HoldingHistory, holding_history
 from .lots import HoldingLot, fifo_holding_lots
+from .redemption import FifoRedemptionAllocation, fifo_redemption_allocations
 from .holding_constraints import HoldingConstraintAnalysis, HoldingTaxConstraint, LotTaxAnalysis, analyze_holding
 from .constraint_factory import holding_constraint_for_fund
 from .holding_availability import HoldingAvailability, LockedLotAvailability, summarize_holding_availability
@@ -23,6 +24,7 @@ __all__ = [
     "build_formation_intent", "reconcile_economically", "reconcile_positions",
     "transactions_for_holding", "HoldingHistory", "holding_history",
     "HoldingLot", "fifo_holding_lots",
+    "FifoRedemptionAllocation", "fifo_redemption_allocations",
     "FundClassification", "TransitionFundMetadata", "classify_fund", "project_fund_metadata",
     "HoldingConstraintAnalysis", "HoldingTaxConstraint", "LotTaxAnalysis", "analyze_holding",
     "holding_constraint_for_fund",
